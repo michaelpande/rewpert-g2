75961cc9a4fa608ab8d47bb6e252f7f01ce2aa6b
PetterOlsen <petterl@hotmail.com>
2015-01-26 15:58:59 +0100

    Updated parse.php to receive more then one newsItem. User is not included in this version

--- parser/parse.php
+++ parser/parse.php
@@ -2,31 +2,43 @@
 
 include('errorLogger.php');
 class Parse {
-
 	
-
-
-
-    public static function createPost($file){
+	public static function createPost($file){
 		$doc = new DOMDocument();
         $doc->loadXML($file); // This is for string not file, file is just $doc->load($file);
 
         $xpath = new DOMXPath($doc);
         $xpath->registerNamespace('html', "http://www.w3.org/1999/xhtml");
-		$xpath->registerNamespace('nitfns', "http://iptc.org/std/NITF/2006-10-18/");
+		$xpath->registerNamespace('nitf', "http://iptc.org/std/NITF/2006-10-18/");
 		$xpath->registerNamespace('newsMessage', "http://iptc.org/std/nar/2006-10-01/");
 		
-		//Parse::getPostDate($xpath);
+		$newsItemList = $xpath->query("//newsMessage:newsItem");
 		
+		$returnArray = array( );
+		
+		foreach($newsItemList as $newsItem) {
+			$newsItemArray = array(
+				'status_code' => '',
+				'post' => Parse::createPostArray($newsItem, $xpath),
+				'meta' => Parse::createMetaArray($newsItem, $xpath)
+			);
+			
+			array_push($returnArray, $newsItemArray);
+		}
+		
+		return $returnArray;	
+	}
+	
+	private static function createPostArray($newsItem, $xpath) {
 		/*
 		An array containing infomation aout content, title, and other pysical infomnation
 		that is containd in a NewsML-G2 document
 		*/
         $post = array(
             //'ID'           => [ <post id> ] // Are you updating an existing post?
-            'post_content'   => Parse::getPostContent($xpath), // The full text of the post.
-            'post_name'      => Parse::getPostSlug($xpath), // The name (slug) for your post
-            'post_title'     => Parse::getPostHeadline($xpath), // The title of your post.
+            'post_content'   => Parse::getPostContent($newsItem, $xpath), // The full text of the post.
+            //'post_name'      => [ 'string' ] // The name (slug) for your post
+            'post_title'     => Parse::getPostHeadline($newsItem, $xpath), // The title of your post.
             'post_status'  	 => 'publish' //[ 'draft' | 'publish' | 'pending'| 'future' | 'private' | custom registered status ] // Default 'draft'.
             /*'post_type'      => [ 'post' | 'page' | 'link' | 'nav_menu_item' | custom post type ] // Default 'post'.
             'post_author'    => [ <user ID> ] // The user ID number of the author. Default is the current user ID.
@@ -48,60 +60,48 @@ class Parse {
             'page_template'  => [ <string> ] // Requires name of template file, eg template.php. Default empty.*/
         );
 		
-		/*
-		An array containig the metadata of a NewsML-G2 document
-		*/
+		return $post;
+	}
+	
+	public static function createMetaArray($newsItem, $xpath) {
 		$meta = array(
-			'nml2_guid' 		  => Parse::getMetaGuid($xpath), //string
-			'nml2_version' 		  => Parse::getMetaVersion($xpath),
-			'nml2_firstCreated'   => Parse::getMetaFirstCreated($xpath),
-			'nml2_versionCreated' => Parse::getMetaVersionCreated($xpath),
-			'nml2_creator'		  => Parse::getMetaCreator($xpath),
-			'nml2_contributor' 	  => Parse::getContributers($xpath)
-		);
-
-		/*
-		The array that are sendt to the RESTApi, containing a response mesage, the post array and the meta array
-		*/
-		$returnArray = array(
-			'response' => "ok", //Response message placheholder, when ready update to: Parse::insertArrayCheck($array);
-			'post'     => $post, // The post object created above
-			'meta'     => $meta //Array whit metadata
+			'nml2_guid' 		  => Parse::getMetaGuid($newsItem, $xpath), //string
+			'nml2_version' 		  => Parse::getMetaVersion($newsItem, $xpath),
+			'nml2_firstCreated'   => Parse::getMetaFirstCreated($newsItem, $xpath),
+			'nml2_versionCreated' => Parse::getMetaVersionCreated($newsItem, $xpath),
 		);
 		
-		return $returnArray;
-    }
-
-	/*
-	Retrieving the content from the NewsML-G2 document
-	*/
-	private static function getPostContent($xpath){
+		return $meta;
+	}
+	
+	private static function getPostContent($newsItem, $xpath) {
 		$content = null;
-		$nodelist = $xpath->query("//html:p");
+		
+		$nodelist = $xpath->query("newsMessage:contentSet/newsMessage:inlineXML/html:html/html:body", $newsItem);
 		
 		if($nodelist->length == 0) {
-			$nodelist = $xpath->query("//nitfns:p");
+			$nodelist = $xpath->query("newsMessage:contentSet/newsMessage:inlineXML/nitf:nitf/nitf:body/nitf:body.content", $newsItem);
 			if($nodelist->length == 0) {
-			$nodelist = $xpath->query("//newsMessage:inlineData");
+			$nodelist = $xpath->query("newsMessage:contentSet/newsMessage:inlineData", $newsItem);
 			}
 		}
 
 		foreach($nodelist as $node) {
-            $content .= $node->nodeValue . "<br/><br/>";
+            $content = $node->nodeValue;
         }
 
         return $content;
-    }
+	}
 	
-	private static function getPostHeadline($xpath) {
+	private static function getPostHeadline($newsItem, $xpath) {
 		$headline = null;
-		$nodelist = $xpath->query("//newsMessage:headline");
+		$nodelist = $xpath->query("newsMessage:contentMeta/newsMessage:headline", $newsItem);
 		
 		if($nodelist->length == 0) {
-			$nodelist = $xpath->query("//html:title");
+			$nodelist = $xpath->query("newsMessage:contentSet/newsMessage:inlineXML/html:html/html:head/html:title", $newsItem);
 			
 			if($nodelist->length == 0) {
-				$nodelist = $xpath->query("//nitfns:h1");
+				$nodelist = $xpath->query("newsMessage:contentSet/newsMessage:inlineXML/nitf:nitf/nitf:body.head/nitf:hedline", $newsItem);
 			}
 		}
 		
@@ -112,20 +112,9 @@ class Parse {
 		return $headline;
 	}
 	
-	private static function getPostSlug($xpath) {
-		$nodelist = $xpath->query("//newsMessage:slugline");
-		$slugline = null;
-	
-		foreach($nodelist as $node) {
-			$slugline = $node->nodeValue;
-		}
-
-		return $slugline;
-	}
-	
-	private static function getMetaGuid($xpath) {
+	private static function getMetaGuid($newsItem, $xpath) {
 		$guid = null;
-		$nodelist = $xpath->query("//newsMessage:newsItem/@guid");
+		$nodelist = $xpath->query("@guid", $newsItem);
 		
 		foreach($nodelist as $node) {
 			$guid = $node->nodeValue;
@@ -134,9 +123,9 @@ class Parse {
 		return $guid;
 	}
 	
-	private static function getMetaVersion($xpath) {
+	private static function getMetaVersion($newsItem, $xpath) {
 		$version = null;
-		$nodelist = $xpath->query("//newsMessage:newsItem/@version");
+		$nodelist = $xpath->query("@version", $newsItem);
 		
 		foreach($nodelist as $node) {
 			$version = $node->nodeValue;
@@ -148,9 +137,9 @@ class Parse {
 	/*
 	Note: this function gives an unchanged string as ansver, not dateTime
 	*/
-	private static function getMetaFirstCreated($xpath) {
+	private static function getMetaFirstCreated($newsItem, $xpath) {
 		$firstCreated = null;
-		$nodelist = $xpath->query("//newsMessage:firstCreated");
+		$nodelist = $xpath->query("newsMessage:itemMeta/newsMessage:firstCreated", $newsItem);
 		
 		foreach($nodelist as $node) {
 			$firstCreated = $node->nodeValue;
@@ -162,9 +151,9 @@ class Parse {
 	/*
 	Note: this function gives an unchanged string as ansver, not dateTime
 	*/
-	private static function getMetaVersionCreated($xpath) {
+	private static function getMetaVersionCreated($newsItem, $xpath) {
 		$versionCreated = null;
-		$nodelist = $xpath->query("//newsMessage:versionCreated");
+		$nodelist = $xpath->query("newsMessage:itemMeta/newsMessage:versionCreated", $newsItem);
 		
 		foreach($nodelist as $node) {
 			$versionCreated = $node->nodeValue;
@@ -172,163 +161,4 @@ class Parse {
 		
 		return $versionCreated;
 	}
-	
-	private static function getMetaCreator($xpath) {
-		$creator = array( );
-		$nodelist = $xpath->query("//newsMessage:creator/newsMessage:name");
-		
-		if($nodelist->length == 0) {
-			foreach($nodelist as $node) {
-				$userdata = array(
-				'user_login'   		 	=> $node->nodeValue, //'login_name',
-				'user_url'     		 	=> "www.placeholder.no", //$website,
-				'user_pass'    		 	=> null,  // When creating an user, `user_pass` is expected.
-				'first_name'   		 	=> $node->nodeValue,
-				'last_name'    		 	=> null,
-				'nickname'     		 	=> null,
-				'description'  		 	=> null,
-				'rich_editing' 		 	=> null,
-				'comment_shortcuts'	 	=> null,
-				'admin_color'		 	=> null,
-				'use_ssl'			 	=> null,
-				'show_admin_bar_front'	=> null
-				);
-				
-				array_push($creator, $userdata);
-				
-				return $creator;
-			}
-			$nodelist = $xpath->query("//newsMessage:creator/@literal");
-		}
-		
-		foreach($nodelist as $node) {
-			$userdata = array(
-				'user_login'   		 	=> $node->nodeValue, //'login_name',
-				'user_url'     		 	=> "www.placeholder.no", //$website,
-				'user_pass'    		 	=> null,  // When creating an user, `user_pass` is expected.
-				'first_name'   		 	=> $node->nodeValue,
-				'last_name'    		 	=> null,
-				'nickname'     		 	=> null,
-				'description'  		 	=> null,
-				'rich_editing' 		 	=> null,
-				'comment_shortcuts'	 	=> null,
-				'admin_color'		 	=> null,
-				'use_ssl'			 	=> null,
-				'show_admin_bar_front'	=> null
-				);
-				
-				array_push($creator, $userdata);
-		}
-		
-		return $creator;
-	}
-	
-	/*private static function getMetaCreator($xpath) {
-		$creator = null;
-		$nodelist = $xpath->query("//newsMessage:creator/newsMessage:name");
-		
-		if($nodelist->length == 0) {
-			$nodelist = $xpath->query("//newsMessage:creator/@literal");
-		}
-		
-		foreach($nodelist as $node) {
-			$creator = $node->nodeValue;
-		}
-		
-		return $creator;
-	}*/
-	
-	/*
-	Checking if any of the infomation in the in post_content and post_title are missing
-	and sending a coresponding response
-	*/
-	private static function insertArrayCheck($array) {
-		if($array[ 'post_content' ] === null and $array[ 'post_title' ] === null) {
-			errorLogger::headerStatus(500);
-			return; 	
-		}
-		else if($array[ 'post_content' ] === null) {
-			errorLogger::HeaderStatus(500);
-			return;
-		}
-		else if($array[ 'post_title' ] === null) {
-			errorLogger::HeaderStatus(500);
-			return;
-		}
-		else {
-			errorLogger:HeaderStatus(200);
-			return;
-		}
-	}
-	
-	/*private static function getPostDate ($xpath) {
-		$nodelist = $xpath->query("//embargoed");
-		
-		$embargoString = '';
-		
-		foreach($nodelist as $node) {
-			$embargoString = $node->nodeValue;
-		}
-		
-		$embargoDate = DateTime::createFromFormat('Y-m-d H:i:s', substr($embargoString, 0, 10) . ' ' . substr($embargoString, 11, 8));
-		
-		echo $embargoDate->format('Y-m-d H:i:s');
-		return $embargoDate;
-	}*/
-	
-
-	private static function getContributers($xpath) {
-		$contributers = array( );
-		
-		$nodelist = $xpath->query("//newsMessage:contributor/@literal");
-		
-		
-		if($nodelist->length != 0) {
-			foreach($nodelist as $node) {
-				$userdata = array(
-				'user_login'   		 	=> $node->nodeValue, //'login_name',
-				'user_url'     		 	=> "www.placeholder.no", //$website,
-				'user_pass'    		 	=> null,  // When creating an user, `user_pass` is expected.
-				'first_name'   		 	=> $node->nodeValue,
-				'last_name'    		 	=> null,
-				'nickname'     		 	=> null,
-				'description'  		 	=> null,
-				'rich_editing' 		 	=> null,
-				'comment_shortcuts'	 	=> null,
-				'admin_color'		 	=> null,
-				'use_ssl'			 	=> null,
-				'show_admin_bar_front'	=> null
-				);
-				
-				array_push($contributers, $userdata);
-			}
-
-			return $contributers;
-		}
-		
-		$nodelist = $xpath->query("//newsMessage:contributor/newsMessage:name");
-		
-		foreach($nodelist as $node) {		
-			$userdata = array(
-				'user_login'   		 	=> str_replace(' ', '', $node->nodeValue), //'login_name',
-				'user_url'     		 	=> "www.placeholder.no", //$website,
-				'user_pass'    		 	=> null,  // When creating an user, `user_pass` is expected.
-				'first_name'   		 	=> $node->nodeValue,
-				'last_name'    		 	=> null,
-				'nickname'     		 	=> null,
-				'description'  		 	=> null,
-				'rich_editing' 		 	=> null,
-				'comment_shortcuts'	 	=> null,
-				'admin_color'		 	=> null,
-				'use_ssl'			 	=> null,
-				'show_admin_bar_front'	=> null
-				);
-				
-			array_push($contributers, $userdata);
-		}
-		
-		return $contributers;
-	}
-
-
 }
