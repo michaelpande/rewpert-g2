2588e28b6795058ededcce4b5eec0ecf1fec718b
PetterOlsen <petterl@hotmail.com>
2015-02-01 17:50:27 +0100

    Added users (creator/contributor) to the parse array

--- parser/parse.php
+++ parser/parse.php
@@ -22,13 +22,27 @@ class Parse {
 								'nml2_versionCreated' 	=> string
 								'nml2_embarogDate' 	  	=> string
 								'nml2_newsMessageSendt' => string
-								'nml2_subject'			=> $subjects = array(
-															 0 => string
-															 1 => string
-															 2 => string
-															 ...(number of subjects my vary)
-														   );
+								'nml2_language'			=> string
 							  );
+					'users' => $users = array(
+								'creators' 	   => $creator = array(
+													0 => $user = array(
+															'user_login'  => string
+															'description' => string
+															'nml2_qude'	  => string
+															'nml2_uri'	  => string
+														  );
+													1 => Same as the index above. Number of indexes dependes on number of creators
+								'contributors' => $contributor = array(
+													0 => $user array(
+															'user_login'  => string
+															'description' => string
+															'nml2_qude'	  => string
+															'nml2_uri'	  => string
+												         );
+													1 => Same as the index above. Number of indexes dependes on number of contributors
+													);
+							   );
 				 );
 			1 => Same as index 0. This is index, index 0 and alle numbers above is added whit array_push
 				 and the index numbers used is decidded by the numbers of newsItems
@@ -40,7 +54,13 @@ class Parse {
 	*/ 	
 	public static function createPost($file){
 		$doc = new DOMDocument();
-		$doc->loadXML($file); // This is for string not file, file is just $doc->load($file);
+		
+		if(is_file($file)) {
+			$doc->load($file);
+		}
+		else {
+			$doc->loadXML($file);
+		}
 
         $xpath = new DOMXPath($doc);
 		
@@ -60,8 +80,9 @@ class Parse {
 		
 		foreach($newsItemList as $newsItem) {
 			$newsItemArray = array(
-				'post' => Parse::createPostArray($newsItem, $xpath),
-				'meta' => Parse::createMetaArray($newsItem, $xpath)
+				'post'  => Parse::createPostArray($newsItem, $xpath),
+				'meta'  => Parse::createMetaArray($newsItem, $xpath),
+				'users' => Parse::createUserArray($newsItem, $xpath)
 			);
 			
 			//Cheking if there is anny errors in the data gathert from the newsML document and chenges status code accordingly
@@ -120,18 +141,29 @@ class Parse {
 		- $newsItem: the result of the query made at the start of the document to separate diferent newsItems
 		- $xpath: the XPath variable neded to peform a query on a XML document
 	*/ 	
-	public static function createMetaArray($newsItem, $xpath) {
+	private static function createMetaArray($newsItem, $xpath) {
 		$meta = array(
 			'nml2_guid' 		  	=> Parse::getMetaGuid($newsItem, $xpath), //string, the guide of the newsItem
 			'nml2_version' 		  	=> Parse::getMetaVersion($newsItem, $xpath), //string, the version of the newsItem
 			'nml2_firstCreated'   	=> Parse::getMetaFirstCreated($newsItem, $xpath), //string, the timesap when the newsItem was first created
 			'nml2_versionCreated' 	=> Parse::getMetaVersionCreated($newsItem, $xpath), //string, the timestamp when the curent version of the newsItem var created
 			'nml2_embarogDate' 	  	=> Parse::getMetaEmbargo($newsItem, $xpath), //string, timestamp of the embargo
-			'nml2_newsMessageSendt' => Parse::getMetaSendtDate($xpath), //string
-			'nml2_subject' 			=> Parse::getMetaSubjects($newsItem, $xpath) //array
+			'nml2_newsMessageSendt' => Parse::getMetaSentDate($xpath), //string
+			'nml2_language'			=> Parse::getMetaLanguage($newsItem, $xpath), //string
+			'nml2_creator' 			=> null, //array
+			'nm2_contributor' 		=> null //array
 		);
 		
 		return $meta;
+	}
+	
+	private static function createUserArray($newsItem, $xpath) {
+		$users = array(
+			'creators' 		=> Parse::getCreator($newsItem, $xpath),
+			'contributors' 	=> Parse::getContributor($newsItem, $xpath)
+		);
+		
+		return $users;
 	}
 	
 	/*A metod used to find the contet of the news articel
@@ -346,6 +378,13 @@ class Parse {
 		- $newsItem: the result of the query made at the start of the document to separate diferent newsItems
 		- $xpath: the XPath variable neded to peform a query on a XML document
 	*/
+	
+	/**
+	 * Findes and returns timestamp from when the present version where created
+	 *
+	 * This metod uses a DOMXPath query to find and return a timestamp from when the curent version of the NewsML-G2
+	 * document where created
+	 */
 	private static function getMetaVersionCreated($newsItem, $xpath) {
 		$versionCreated = null;
 		
@@ -364,13 +403,15 @@ class Parse {
 		return $versionCreated;
 	}
 	
-	/*A method used to find the timestamp that specifies when the embargo is set to
-	  This metod does not change the timestamp in any way, just fetches it from the XML and send it ot the RESTapi
-	  Returns: timestamp as string, null if no embargoed tag found
-	  Parameters: 
-		- $newsItem: the result of the query made at the start of the document to separate diferent newsItems
-		- $xpath: the XPath variable neded to peform a query on a XML document
-	*/
+	/**
+	 * Finds and returns the embargo if present
+	 *
+	 * This metod user DOMXPath query to find the embargo date of a NewsML-G2 Docuemtn and reutrns it as a string
+	 *
+	 * @param DOMNodeList $newsItem XPath query from an erlier part of the document that the new query shal be preformed eon
+	 * @param DOMXpath $xpath Used to find information in a NewsML-G2 document
+	 * @return string embargo date, null of no embargo i present
+	 */
 	private static function getMetaEmbargo($newsItem, $xpath) {
 		$embargo = null;
 		
@@ -388,15 +429,19 @@ class Parse {
 		
 		return $embargo;
 	}
-	
-	/*A metod used to find the timestamp fhrome when the newsMessage where sendt
-	  This metod does not change the timestamp in any way, just fetches it from the XML and send it ot the RESTapi
-	  Returns: timestamp as string, null if no sendt date found
-	  Parameter: 
-		- $xpath: the XPath variable neded to peform a query on a XML document
-	*/
-	private static function getMetaSendtDate($xpath) {
-		$dateSendt = null;
+
+	/**
+	 * Finds and returns the sent date frome the NewsML document
+	 *
+	 * This metod finds the <sent> tag in NewsML-G2 and returns it as a astring. It uses DOMEXpath
+	 * find the tag
+	 *
+	 * @param DOMXpath $xpath Used to find information in a NewsML-G2 document
+	 * @return string Date sent, null if no date is present
+	 * @author Petter Lundberg Olsen
+	 */ 
+	private static function getMetaSentDate($xpath) {
+		$dateSent = null;
 		
 		//Path without XML namespace: newsMessage/header/sent
 		$nodelist = $xpath->query("//newsMessage:newsMessage/newsMessage:header/newsMessage:sent");
@@ -405,11 +450,125 @@ class Parse {
 		  The length of the $nodelist shuld only be 1 if the newsML is created correctly.
 		*/
 		foreach($nodelist as $node) {
-			$dateSendt = $node->nodeValue;
+			$dateSent = $node->nodeValue;
 		}
 		
-		return $dateSendt;
+		return $dateSent;
 	}
+	
+	/**
+	 * Finds and returns the language of the news aritcel
+	 *
+	 * This metod findes the laguage of the content in a NewsML-G2 docuemnt using DOMXPath and returns it as a string
+	 *
+	 * @param DOMNodeList $newsItem XPath query from an erlier part of the document that the new query shal be preformed eon
+	 * @param DOMXpath $xpath Used to find information in a NewsML-G2 document
+	 * @return string The laguage of the news articel
+	 * @author Petter Lundberg Olsen
+	 */
+	private static function getMetaLanguage($newsItem, $xpath) {
+		$language = null;
+		
+		$nodelist = $xpath->query("newsMessage:contentMeta/newsMessage:language/@tag", $newsItem);
+		
+		foreach($nodelist as $node) {
+			$language = $node->nodeValue;
+		}
+		
+		return $language;
+	}
+	
+	private static function getCreator($newsItem, $xpath) {
+		$creator = array ( );
+		
+		$nodelist = $xpath->query("newsMessage:contentMeta/newsMessage:creator", $newsItem);
+		
+		foreach($nodelist as $node) {
+			$user = array(
+				'user_login' 	=> Parse::getUserName($node, $xpath),
+				'description'	=> Parse::getUserDescription($node, $xpath),
+				'nml2_qcode'		=> Parse::getUserQcode($node, $xpath),
+				'nml2_uri'		=> Parse::getUserUri($node, $xpath)
+			);
+
+			array_push($creator, $user);
+		}
+		
+		return $creator;
+	}
+	
+	private static function getContributor($newsItem, $xpath) {
+		$contributor = array( );
+		
+		$nodelist = $xpath->query("newsMessage:contentMeta/newsMessage:contributor", $newsItem);
+		
+		foreach($nodelist as $node) {
+			$user = array(
+				'user_login' 	=> Parse::getUserName($node, $xpath),
+				'description'	=> Parse::getUserDescription($node, $xpath),
+				'nml2_qude'		=> Parse::getUserQcode($node, $xpath),
+				'nml2_uri'		=> Parse::getUserUri($node, $xpath)
+			);
+			
+			array_push($contributor, $user);
+		}
+		
+		return $contributor;
+	}
+	
+	private static function getUserName($cTag, $xpath) {
+		$userName = null;
+		
+		$nodelist = $xpath->query("newsMessage:name", $cTag);
+		
+		if($nodelist->length == 0) {
+			$nodelist = $xpath->query("@literal", $cTag);
+		}
+		
+		foreach($nodelist as $node) {
+			$userName = $node->nodeValue;
+		}
+		
+		return $userName;
+	}
+	
+	private static function getUserDescription($cTag, $xpath) {
+		$description = null;
+		
+		$nodelist = $xpath->query("@role", $cTag);
+		
+		foreach($nodelist as $node) {
+			$description = $node->nodeValue;
+		}
+		
+		return $description;
+	}
+	
+	private static function getUserQcode($cTag, $xpath) {
+		$qcode = null;
+		
+		$nodelist = $xpath->query("@qcode", $cTag);
+		
+		foreach($nodelist as $node) {
+			$qcode = $node->nodeValue;
+		}
+		
+		return $qcode;
+	}
+	
+	private static function getUserUri($cTag, $xpath) {
+		$uri = null;
+		
+		$nodelist = $xpath->query("@uri", $cTag);
+		
+		foreach($nodelist as $node) {
+			$uri = $node->nodeValue;
+		}
+		
+		return $uri;
+	}
+		
+		
 	
 	/*A method used to find the subjects in the NewsML article
 	  Returns: array containing subjects, empty array if no subject is found
@@ -421,12 +580,23 @@ class Parse {
 		$subjects = array ( );
 		
 		/*Query path that continus from first query at the start of the document.
-		  Path without XML namespace: contentMeta/subject/@qcode (fetching qcode forom the subject tag)
+		  Path without XML namespace: contentMeta/subject/name
 		*/
-		$nodelist = $xpath->query("newsMessage:contentMeta/newsMessage:subject/@qcode", $newsItem);
+		$nodelist = $xpath->query("newsMessage:contentMeta/newsMessage:subject/newsMessage:name", $newsItem);
 		
-		//eners this loop if any subject is found
+		//Pushes the subjects found in the query on to the end of the array
 		foreach($nodelist as $node) {
+			array_push($subjects, $node->nodeValue);
+		}
+
+		
+		//If noe subject where found in the first query enter this part of the code
+		if(count($subjects) == 0) {
+		
+			/*Query path that continus from first query at the start of the document.
+			  Path without XML namespace: contentMeta/subject/@qcode (fetching qcode forom the subject tag)
+			*/
+			$nodelist = $xpath->query("newsMessage:contentMeta/newsMessage:subject/@qcode", $newsItem);
 			
 			/*pushes the value of the subject on the end of the array if the value matches the regula expresion
 			  Exsamples that will pas the regex: subj:11000000, subj:10000000, subj:04007000
@@ -436,30 +606,21 @@ class Parse {
 			}
 		}
 		
-		//If noe subject where found in the first query enter this part of the code
-		if(count($subjects) == 0) {
-			
-			/*Query path that continus from first query at the start of the document.
-			  Path without XML namespace: contentMeta/subject/name
-			*/
-			$nodelist = $xpath->query("newsMessage:contentMeta/newsMessage:subject/newsMessage:name", $newsItem);
-			
-			//Pushes the subjects found in the query on to the end of the array
-			foreach($nodelist as $node) {
-				array_push($subjects, $node->nodeValue);
-			}
-		}
-		
 		return $subjects;
 	}
 	
-	/*A metod that goes through the vital part of the data that has bean colected frome the NewsML document
-	  Returns: status code as in. 200 if everthing is OK, 400  if shoething is missing or the curent 
-			   status code if it is not 200
-	  Parameter: 
-		- $returnArray: array containing the data gatherd from the NewsML document
-		- $newsItemArray: array containing the status code to check what it is curently set to
-	*/
+	/**
+	 * Checks if some of the parts of the data being sendt to Wordpress is mising and setting status code acordingly
+	 *
+	 * Checks first if 'status_code' in $returnArray is set to something diferent then 200 and returns that number if it dose.
+	 * Checks then if any of the more inportant parts of the meta and post arrays are missing, and if the are returning 400.
+	 * The metod returns 200 if everthing is OK
+	 *
+	 * @param array $returnArray The array containg 'status_code'
+	 * @param array $newsItemArray The array holding all the data that are to be checked
+	 * @return int 200 if all OK, 400 if something is missing and 'status_code' value if not 200
+	 * @author Petter Lundberg Olsen
+	 */
 	private static function setStatusCode($returnArray, $newsItemArray) {
 		if($returnArray['status_code'] != 200) {
 			return $returnArray['status_code'];
@@ -487,10 +648,17 @@ class Parse {
 
 		return 200;
 	} 
-	
-	/*Metod to change 'post_status' in post array if embargo date is found in NewsML document.
-	  Returns: 'future' if embargo is present, else 'publish'
-	*/
+
+	/**
+	 * A metod that return ether 'publish' or 'future' depending in embargo.
+	 * 
+	 * This metod is used to change the 'post_status' in the $post array. Returns if 'publish' if
+	 * $embargo is set. Returns 'future' if $embargo is null.
+	 * 
+	 * @param string $embargo Embargo date as string, may be null
+	 * @reutrn string 'publish' or 'future'
+	 * @author Petter Lundberg Olsen
+	 */
 	public static function setEbargoState($embargo) {
 		if($embargo === null) {
 			return 'publish';
