21797cfcbd703c31d887c62c3096af5daceb1a39
PetterOlsen <petterl@hotmail.com>
2015-02-10 14:18:09 +0100

    Changed users from two arrays (creators and contributors) to one user array

--- parser/newsItemParse.php
+++ parser/newsItemParse.php
@@ -189,29 +189,11 @@ class NewsItemParse {
 	 */
 	private static function createPostArray($newsItem, $xpath) {
         $post = array(
-            //'ID'           => [ <post id> ] // Are you updating an existing post?
             'post_content'   => NewsItemParse::getPostContent($newsItem, $xpath), // The full text of the post.
             'post_name'      => NewsItemParse::getPostName($newsItem, $xpath), // The name (slug) for your post
             'post_title'     => NewsItemParse::getPostHeadline($newsItem, $xpath), // The title of your post.
             'post_status'  	 => 'publish', //[ 'draft' | 'publish' | 'pending'| 'future' | 'private' | custom registered status ] // Default 'draft'.
-            /*'post_type'      => [ 'post' | 'page' | 'link' | 'nav_menu_item' | custom post type ] // Default 'post'.
-            'post_author'    => [ <user ID> ] // The user ID number of the author. Default is the current user ID.
-            'ping_status'    => 'closed',// Pingbacks or trackbacks allowed. Default is the option 'default_ping_status'.
-            'post_parent'    => [ <post ID> ] // Sets the parent of the new post, if any. Default 0.
-            'menu_order'     => [ <order> ] // If new post is a page, sets the order in which it should appear in supported menus. Default 0.
-            'to_ping'        => // Space or carriage return-separated list of URLs to ping. Default empty string.
-            'pinged'         => // Space or carriage return-separated list of URLs that have been pinged. Default empty string.
-            'post_password'  => [ <string> ] // Password for post, if any. Default empty string.
-            'guid'           => NewsItemParse::getPostGuid($xpath)// Skip this and let Wordpress handle it, usually.
-            'post_content_filtered' => // Skip this and let Wordpress handle it, usually.
-            'post_excerpt'   => [ <string> ] // For all your post excerpt needs.
-            'post_date'      => [ Y-m-d H:i:s ] // The time post was made.
-            'post_date_gmt'  => [ Y-m-d H:i:s ] // The time post was made, in GMT.
-            'comment_status' => [ 'closed' | 'open' ] // Default is the option 'default_comment_status', or 'closed'.
-            'post_category'  => [ array(<category id>, ...) ] // Default empty.*/
             'tags_input'     => NewsItemParse::getPostTags($newsItem, $xpath) // Default empty.
-            /*'tax_input'      => [ array( <taxonomy> => <array | string> ) ] // For custom taxonomies. Default empty.
-            'page_template'  => [ <string> ] // Requires name of template file, eg template.php. Default empty.*/
         );
 		
 		return $post;
@@ -252,10 +234,37 @@ class NewsItemParse {
 	 * @author Petter Lundberg Olsen
 	 */
 	private static function createUserArray($newsItem, $xpath) {
-		$users = array(
-			'creators' 		=> NewsItemParse::getCreator($newsItem, $xpath), //array
-			'contributors' 	=> NewsItemParse::getContributor($newsItem, $xpath) //array
-		);
+		$users = array( );
+		
+		$nodelist = $xpath->query("newsMessage:contentMeta/newsMessage:creator", $newsItem);
+		
+		foreach($nodelist as $node) {
+			$user = array(
+				'user_login' 	=> NewsItemParse::getUserName($node, $xpath), //string login_name of the user
+				'description'	=> NewsItemParse::getUserDescription($node, $xpath), //string, descibing the role of the user
+				'user_email'	=> NewsItemParse::getUserEmail($node, $xpath),
+				'nml2_qcode'	=> NewsItemParse::getUserQcode($node, $xpath), //string, the users NewsML-G2 qcode
+				'nml2_uri'		=> NewsItemParse::getUserUri($node, $xpath) //string, the users NewsML-G2 uri
+			);
+			
+			array_push($users, $user);
+		}
+		
+		$nodelist = $xpath->query("newsMessage:contentMeta/newsMessage:contributor", $newsItem);
+		
+		foreach($nodelist as $node) {
+			$user = array(
+				'user_login' 	=> NewsItemParse::getUserName($node, $xpath), //string login_name of the user
+				'description'	=> NewsItemParse::getUserDescription($node, $xpath), //string, descibing the role of the user
+				'user_email'	=> NewsItemParse::getUserEmail($node, $xpath),
+				'nml2_qude'		=> NewsItemParse::getUserQcode($node, $xpath), //string, the users NewsML-G2 qcode
+				'nml2_uri'		=> NewsItemParse::getUserUri($node, $xpath) //string, the users NewsML-G2 uri
+			);
+			
+			array_push($users, $user);
+		}
+		
+		var_dump($users);
 		
 		return $users;
 	}
@@ -673,79 +682,7 @@ class NewsItemParse {
 		
 		return $language;
 	}
-	
-	/**
-	 * Creates and returns all the creators of a newsItem
-	 *
-	 * This metod uses a DOMXPath query to find all the creators of a newsItem. It then files a user array whit iformation neded
-	 * to add a user in wordpress and some other metadata
-	 *
-	 * @param DOMNode $newsItem XPath query result from an erlier part of the document that the new query shal be preformed on
-	 * @param DOMXpath $xpath Used to find information in a NewsML-G2 document
-	 * @return array contaning all creators
-	 * @author Petter Lundberg Olsen
-	 */
-	private static function getCreator($newsItem, $xpath) {
-		$creator = array ( );
-		
-		/*Query path that continus from first query at the start of the document.
-		  Path without XML namespace: contentMeta/creator
-		*/
-		$nodelist = $xpath->query("newsMessage:contentMeta/newsMessage:creator", $newsItem);
-		
-		//Takes each creator and adds ther information in the a user array
-		foreach($nodelist as $node) {
-			$user = array(
-				'user_login' 	=> NewsItemParse::getUserName($node, $xpath), //string login_name of the user
-				'description'	=> NewsItemParse::getUserDescription($node, $xpath), //string, descibing the role of the user
-				'user_email'	=> NewsItemParse::getUserEmail($node, $xpath),
-				'nml2_qcode'	=> NewsItemParse::getUserQcode($node, $xpath), //string, the users NewsML-G2 qcode
-				'nml2_uri'		=> NewsItemParse::getUserUri($node, $xpath) //string, the users NewsML-G2 uri
-			);
 
-			array_push($creator, $user);
-		}
-		
-		return $creator;
-	}
-	
-	/**
-	 * Creates and returns all the contributors of a newsItem
-	 *
-	 * This metod uses a DOMXPath query to find all the contributors of a newsItem. It then files a user array whit iformation neded
-	 * to add a user in wordpress and some other metadata
-	 *
-	 * @param DOMNode $newsItem XPath query from an erlier part of the document that the new query shal be preformed on
-	 * @param DOMXpath $xpath Used to find information in a NewsML-G2 document
-	 * @return array contaning all contributors
-	 * @author Petter Lundberg Olsen
-	 */
-	private static function getContributor($newsItem, $xpath) {
-		$contributor = array( );
-		
-		/*Query path that continus from first query at the start of the document.
-		  Path without XML namespace: contentMeta/contriubtor
-		*/
-		$nodelist = $xpath->query("newsMessage:contentMeta/newsMessage:contributor", $newsItem);
-		
-		//Takes each creator and adds ther information in the a user array
-		foreach($nodelist as $node) {
-			$user = array(
-				'user_login' 	=> NewsItemParse::getUserName($node, $xpath), //string login_name of the user
-				'description'	=> NewsItemParse::getUserDescription($node, $xpath), //string, descibing the role of the user
-				'user_email'	=> NewsItemParse::getUserEmail($node, $xpath),
-				'nml2_qude'		=> NewsItemParse::getUserQcode($node, $xpath), //string, the users NewsML-G2 qcode
-				'nml2_uri'		=> NewsItemParse::getUserUri($node, $xpath) //string, the users NewsML-G2 uri
-			);
-			
-			var_dump($user);
-			
-			array_push($contributor, $user);
-		}
-		
-		return $contributor;
-	}
-	
 	/**
 	 * Find and returns the name of a creator/contributor
 	 *
@@ -815,10 +752,6 @@ class NewsItemParse {
 		$email = null;
 		
 		$nodelist = $xpath->query("newsMessage:personDetails/newsMessage:contactInfo/newsMessage:email", $cTag);
-		
-		if($nodelist->length == 0) {
-			return "test";
-		}
 		
 		foreach($nodelist as $node) {
 			$email = $node->nodeValue;
