e75e490799abe21b113f1ffb19a2f1e6ebe07f6d
Diego Pasten <diego.pasten@gmail.com>
2015-03-04 12:50:59 +0100

    Fix site

---
+++ docu/livedemo.php
@@ -0,0 +1,13 @@
+<link href='http://fonts.googleapis.com/css?family=Droid+Sans|Lato|Oswald|Merriweather|Open+Sans|Roboto+Slab|Roboto:400,,200,100|Roboto+Condensed' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+<link rel="stylesheet" href="css/main.css" type="text/css">
+<script src="js/bootstrap.min.js"></script>
+<?php include("menu.php"); ?>
+<br>
+<br>
+<br>
+<p>Link to openshift</p>
+
+
+</body>
+</html>
