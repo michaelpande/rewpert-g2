24e1789e418914512a7806e8f3410ce3ef5646dc
PetterOlsen <petterl@hotmail.com>
2015-03-24 12:14:40 +0100

    Added DateParserTest to the unit test manager

--- unit_test/unitTestManager.php
+++ unit_test/unitTestManager.php
@@ -4,13 +4,23 @@ class unitTestManager {
 	
 	public static function performUnitTest() {
 		require("newsitemParseTest.php");
+		require("DateParserTest.php");
+		echo "<h3>Starting unit test";
 		
-		$testsPerfomed = 2;
+		$class = new ReflectionClass('newsitemParseTest');
+		$methods = $class->getMethods(ReflectionMethod::IS_PRIVATE);
+		$testsPerfomed += count($methods);
+		
+		$class = new ReflectionClass('DateParserTest');
+		$methods = $class->getMethods(ReflectionMethod::IS_PRIVATE);
+		$testsPerfomed += count($methods) -1;
+		
 		$successful = 0;
 		
 		$successful += newsitemParseTest::runAllTests();
+		$successful += DateParserTest::runAllTests();
 		
-		echo "<h3>" . $successful . "/" . $testsPerfomed . " successful tests</h3>";
+		echo "<h3>" . $successful . "/" . $testsPerfomed . " successful test methods</h3>";
 	}
 }
 
